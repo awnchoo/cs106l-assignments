40102898ec8d459a49f2644cbe96635e404cc186
Choooo <102294285+awnchoo@users.noreply.github.com>
2022-04-17 11:23:35 +0800

    Add files via upload

--- ass3:KDTree/KDTree.h
+++ ass3:KDTree/KDTree.h
@@ -17,7 +17,13 @@
 // including it here so that you may use things like size_t without having to
 // type std::size_t every time.
 using namespace std;
-
+template <size_t N, typename ElemType>
+struct kdtree{
+    Point<N> key;
+    ElemType value;
+    kdtree* left;
+    kdtree* right;
+};
 template <size_t N, typename ElemType>
 class KDTree {
 public:
@@ -108,35 +114,33 @@ public:
     ElemType kNNValue(const Point<N>& key, size_t k) const;
 
 private:
-    struct kdtree{
-        Point<N> key;
-        ElemType value;
-        kdtree* left;
-        kdtree* right;
-    };
-    void freetree(kdtree* );
-    kdtree* findnode(const Point<N>& pt) const {
-        kdtree* node=root;
-        size_t level=0;
-        while(node!=nullptr&&node->key!=pt){
+    void freetree(kdtree<N,ElemType>* );
+    kdtree<N,ElemType>* copy(kdtree<N,ElemType>* other);
+    kdtree<N,ElemType>* findnode(const Point<N>& pt) const;
+    void kNNHelper(BoundedPQueue<ElemType>& bpq,const Point<N>& key,const kdtree<N,ElemType>* node,int level) const;
+    struct kdtree<N,ElemType>* root;
+    size_t _size;
+};
 
-            if(pt[level%N]<node->key[level%N]){
+/** KDTree class implementation details */
+//part one
+template <size_t N, typename ElemType>
+kdtree<N,ElemType>* KDTree<N, ElemType>::findnode(const Point<N>& pt) const {
+     kdtree<N,ElemType>* node=root;
+     size_t level=0;
+     while(node!=nullptr&&node->key!=pt){
+           if(pt[level%N]<node->key[level%N]){
                 node=node->left;
             }else{
                 node=node->right;
             }
             level++;
-        }
-        if(node==nullptr){
+     }
+     if(node==nullptr){
             return nullptr;
-        }
-        return node;
-    };
-    struct kdtree* root;
-    size_t _size;
-};
-
-/** KDTree class implementation details */
+     }
+            return node;
+}
 
 template <size_t N, typename ElemType>
 KDTree<N, ElemType>::KDTree():_size(0) ,root(nullptr){};
@@ -147,7 +151,7 @@ KDTree<N, ElemType>::~KDTree() {
 }
 
 template <size_t N, typename ElemType>
-void KDTree<N, ElemType>::freetree(kdtree* node){
+void KDTree<N, ElemType>::freetree(kdtree<N,ElemType>* node){
     if(node!=nullptr){
         freetree(node->left);
         freetree(node->right);
@@ -175,12 +179,12 @@ void KDTree<N,ElemType>::insert (const Point<N>& pt,const ElemType& value){
     if(contains(pt)){
         findnode(pt)->value=value;
     }else{
-        kdtree* newnode = new kdtree{pt,value,nullptr,nullptr};
+        kdtree<N,ElemType>* newnode = new kdtree<N,ElemType>{pt,value,nullptr,nullptr};
         if(root==nullptr){
             root=newnode;
             _size++;
         }else{
-                kdtree* curr=root;
+                kdtree<N,ElemType>* curr=root;
                 size_t level=0;
                 while(curr!=nullptr){
                     if((newnode->key)[level%N]<(curr->key)[level%N]){
@@ -225,7 +229,7 @@ ElemType& KDTree<N, ElemType>::at(const Point<N>& pt){
     if(findnode(pt)==nullptr){
          throw out_of_range("out of range");
     }
-    return findnode(pt)->value;
+    return static_cast<ElemType&>(findnode(pt)->value);
 }
 
 template<size_t N,typename ElemType>
@@ -233,7 +237,104 @@ const ElemType& KDTree<N, ElemType>::at(const Point<N>& pt) const{
     if(findnode(pt)==nullptr){
          throw out_of_range("out of range");
     }
-    return findnode(pt)->value;
+    return const_cast<ElemType&>(findnode(pt)->value);
+}
+//part two
+
+template<size_t N,typename ElemType>
+ElemType KDTree<N, ElemType>::kNNValue(const Point<N>& key, size_t k) const{
+    BoundedPQueue<ElemType> bpq(k);
+    //find kNN
+    kNNHelper(bpq,key,root,0);
+    //return
+    map<ElemType,int> freqMap;
+    ElemType temp;
+    int maxcount = -1;
+    ElemType ret;
+    while(!bpq.empty()){
+        temp=bpq.dequeueMin();
+        freqMap[temp]+=1;
+        if(freqMap[temp]>maxcount){
+            maxcount = freqMap[temp];
+            ret=temp;
+
+        }
+    }
+    return ret;
+};
+template<size_t N,typename ElemType>
+void KDTree<N, ElemType>::kNNHelper
+(BoundedPQueue<ElemType>& bpq,const Point<N>& key,const kdtree<N,ElemType>* node,int level) const{
+    if(node==nullptr){
+        return;
+    }
+    //figure out the distance
+    double distance=0;
+    for(int j =0;j<N;j++){
+        distance+=pow((key[j]-node->key[j]),2);
+    }
+    distance = sqrt(distance);
+    //enqueue current point
+    bpq.enqueue(node->value,distance);
+    // Recursively search the half of the tree that contains the test point.
+        if(key[level%N]<node->key[level%N]){
+            kNNHelper(bpq,key,node->left,level+1);
+            if(bpq.size()<bpq.maxSize()||fabs(node->key[level%N]-key[level%N])<bpq.worst()){
+                kNNHelper(bpq,key,node->right,level+1);
+            }
+        }else{
+            kNNHelper(bpq,key,node->right,level+1);
+            if(bpq.size()<bpq.maxSize()||fabs(node->key[level%N]-key[level%N])<bpq.worst()){
+                kNNHelper(bpq,key,node->left,level+1);
+            }
+        }
+};
+
+//part three
+template<size_t N,typename ElemType>
+KDTree<N, ElemType>::KDTree(const KDTree& other){
+   _size=other._size;
+   root=copy(other.root);
+}
+
+template<size_t N,typename ElemType>
+KDTree<N,ElemType>& KDTree<N, ElemType>::operator=(const KDTree& rhs){
+    if(this==&rhs){
+        return *this;
+    }
+    freetree(this->root);
+    this->root=copy(rhs.root);
+    this->_size=rhs._size;
+    return *this;
+}
+
+template<size_t N,typename ElemType>
+kdtree<N,ElemType>* KDTree<N, ElemType>::copy(kdtree<N,ElemType>* other){
+    if(other ==nullptr){
+        return nullptr;
+    }
+    kdtree<N,ElemType>* node = nullptr;
+    node = new kdtree<N,ElemType>{other->key,other->value,other->left,other->right};
+    node->left=copy(other->left);
+    node->right=copy(other->right);
+    return node;
+}
+
+template<size_t N,typename ElemType>
+KDTree<N, ElemType>::KDTree(KDTree&& other){
+    _size=other._size;
+    root=copy(other.root);
+}
+
+template<size_t N,typename ElemType>
+KDTree<N,ElemType>& KDTree<N, ElemType>::operator=(KDTree&& rhs){
+    if(this==&rhs){
+        return *this;
+    }
+    freetree(this->root);
+    this->root=copy(rhs.root);
+    this->_size=rhs._size;
+    return *this;
 }
 
 #endif // KDTREE_INCLUDED
